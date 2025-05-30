e8be3a99621e2fe337f21456b13645399fac6caf
Hrlslmn <sulaimanhairul@gmail.com>
2025-05-31 01:10:59 +0800

    revamp purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import ProductPage from './pages/Product';
-import ChatWithAI from './pages/ChatWithAI';
+
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
-import TodoPage from './pages/TodoPage';
 import PrivateRoute from './components/PrivateRoute';
 import SuccessPage from './pages/Success';
 import CancelPage from './pages/Cancel';
+import Dashboard2 from './components/Overview';
+
+// New Sidebar pages
+import ComponentsPage from './pages/ComponentsPage';
+import DesignsPage from './pages/DesignsPage';
+import ThemesPage from './pages/ThemesPage';
+import CollectionsPage from './pages/CollectionsPage';
 
 export default function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/cancel" element={<CancelPage />} />
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
 
-          {/* Protected routes */}
-          <Route path="/" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/products" element={<ProductPage />} />
-          <Route path="/chat" element={<PrivateRoute><ChatWithAI /></PrivateRoute>} />
-          <Route path="/todo" element={<PrivateRoute><TodoPage /></PrivateRoute>} />
+        {/* Sidebar routes */}
+
+        <Route path="/" element={<PrivateRoute><Dashboard2 /></PrivateRoute>} />
+        <Route path="/components" element={<PrivateRoute><ComponentsPage /></PrivateRoute>} />
+        <Route path="/designs" element={<PrivateRoute><DesignsPage /></PrivateRoute>} />
+        <Route path="/themes" element={<PrivateRoute><ThemesPage /></PrivateRoute>} />
+        <Route path="/collections" element={<PrivateRoute><CollectionsPage /></PrivateRoute>} />
         </Routes>
-      </Router>
+    </Router>
   );
 }
-
