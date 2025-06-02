a6b9d548bd6b0bea69ca335a0a91684c96c58f2a
Hrlslmn <sulaimanhairul@gmail.com>
2025-06-02 17:56:27 +0800

    Added speedcheck-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
@@ -41,7 +41,7 @@ export default function App() {
         <Route path="/components/navigation" element={<PrivateRoute><NavigationPage /></PrivateRoute>} />
         <Route path="/components/forms" element={<PrivateRoute><FormsPage /></PrivateRoute>} />
 
-        <Route path="/" element={<PrivateRoute><SpeedInsights><Overview /></SpeedInsights></PrivateRoute>} />
+        <Route path="/" element={<PrivateRoute><SpeedInsights /><Overview /></PrivateRoute>} />
         <Route path="/components" element={<PrivateRoute><ComponentsPage /></PrivateRoute>} />
         <Route path="/designs" element={<PrivateRoute><DesignsPage /></PrivateRoute>} />
         <Route path="/themes" element={<PrivateRoute><ThemesPage /></PrivateRoute>} />
