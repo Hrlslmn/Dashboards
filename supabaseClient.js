9db6945da27edc9490e0158b414dcd440be6a640
Hairul_Sulaiman <sulaimanhairul@gmail.com>
2025-06-03 23:39:52 +0800

    handle download and implement session storage

--- supabaseClient.js
+++ supabaseClient.js
@@ -6,6 +6,7 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
+    storage: sessionStorage, // ⬅️ Switch from localStorage to sessionStorage
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
